b021d7695bf498a704467e8af53601199b6bc1a1
santosh <styadav018@gmail.com>
2023-01-10 17:24:32 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import Slider from "./Slider";
 
 function App() {
   return (
-    <div >
-    <h1>Crousel Slider!!!!!!</h1>
+    <div className="w-[80%] m-auto">
+    <div className="text-5xl text-center mt-4 border-4 w-[80%] p-4 m-auto font-bold border-cyan-500 br text-cyan-300">
+    <marquee  
+    scrollamount="20"
+    behavior="alternate"
+   direction="right" >Crousel Slider!!!!!! Created By Santosh </marquee>
+    </div>
     <Slider/>
      </div>
   );
--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { useCallback } from 'react';
+import {BsChevronCompactLeft,BsChevronCompactRight} from "react-icons/bs";
+import {RxDotFilled} from "react-icons/rx"
 
 const Slider = () => {
+
     const slide =[
         {
             url: 'https://images.unsplash.com/photo-1584221428599-35de2deb7ab1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8MTA4MHB8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
@@ -17,13 +21,49 @@ const Slider = () => {
         {
             url: 'https://images.unsplash.com/photo-1528988296565-3cd70e150617?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fDEwODBwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
         },
-    ]
-  return <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
-    <div style={{backgroundImage: `url(${slide[1].url})`}} className='w-full h-full duration-500 bg-cover rounded-2xl '>
+    ];
+    const [currentIndex,setCurrentIndex] = useState(0)
+
+    const prevSlide=useCallback(()=>{
+        const isFirstSlide = currentIndex === 0;
+        const prevSlide = isFirstSlide ? slide.length-1 : currentIndex -1;
+        setCurrentIndex(prevSlide)
+    },[currentIndex])
+    const nextSlide=useCallback(()=>{
+        const isLastSlide = currentIndex === slide.length-1;
+        const nextSlide = isLastSlide ? 0 : currentIndex +1;
+        setCurrentIndex(nextSlide)
+    },[currentIndex])
+
+    const goToSlide=(slideIndex)=>{
+        setCurrentIndex(slideIndex)
+    }
+    
+  return <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group hover:scale-110 duration-500'>
+    <div style={{backgroundImage: `url(${slide[currentIndex].url})`}} className='w-full h-full duration-1000 bg-cover rounded-2xl'>
     
     </div>
 
-    </div
+   {/* Left Arrow */}
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+    <BsChevronCompactLeft onClick={prevSlide} size={30} />
+    </div>
+
+    {/*Right Arrow*/}
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+    <BsChevronCompactRight size={30} onClick={nextSlide} />
+    </div>
+   <div className='flex justify-center py-2 top-4'>
+    
+   {
+    slide.map((slide,slideIndex)=>(
+        <div key={slideIndex} className='text-5xl cursor-pointer ' onClick={()=>goToSlide(slideIndex)}>
+        <RxDotFilled />
+        </div>
+    ))
+}
+   </div>
+    </div>
 
 
 }
